ac73abb601bf2df032756521f7abb46b10c2490e
Code-Theft <56438939+Code-Theft@users.noreply.github.com>
2021-06-02 00:55:13 +0530

    comp

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e384e1d9887663225b0f93eeea187dd4e8a1da7e',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e384e1d9887663225b0f93eeea187dd4e8a1da7e',
+    ),
+  ),
+);
